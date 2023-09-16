3814c79749161699fde17ecb645bddb616ec9380
liang-faan <liang.faan@gmail.com>
2023-09-16 10:27:47 +0800

    Update

---
+++ neo4j-harness-server/src/test/java/com/lfa/neo4j/harness/HarnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfa.neo4j.harness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
